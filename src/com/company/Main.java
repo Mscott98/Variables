957da19371749430e207fdfe464eb45a559682a6
mariya <mariyascott136@gmail.com>
2019-02-14 00:11:30 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Byte demo;
+        demo = 3;
+
+        short demo2;
+        demo2 = 5;
+
+        int demo3;
+        demo3 = 54;
+
+        long demo4;
+        demo4 = 54545454;
+
+        double demo5;
+        demo5 = 3.14;
+
+        boolean demo6;
+        demo6 = true;
+
+        char demo7;
+        demo7 = m;
+
+        System.out.println(demo, demo2, demo3, demo4, demo5,);
+
+    }
+}
